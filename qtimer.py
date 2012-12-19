5707c73d2649e03fb13f276b742d7add64bbc895
Jon Monroe <berkona@gmail.com>
2012-12-19 14:55:22 -0500

    Added execute permission to qtimer.py, renamed main class to be consistent with script name, added project caching

--- qtimer.py
+++ qtimer.py
@@ -10,12 +10,12 @@ from activecollab.library import ACRequest
 from datetime import datetime, timedelta
 from os import path, makedirs
 
-DB_VERSION = 3
+DB_VERSION = 6
 CONFIG_NAME = 'qtimer.json'
 DATA_NAME = 'timers v%d.db' % DB_VERSION
 
 
-class PyTimer:
+class QTimer:
     def __init__(self, config):
         for n, v in vars(config).items():
             setattr(self, n, v)
@@ -29,6 +29,8 @@ class PyTimer:
 
         self.token = None
         self.url = None
+        self.cacheFunction = None
+        self.lastSynced = None
 
         if not path.exists(self.configPath):
             return
@@ -37,11 +39,12 @@ class PyTimer:
             userConfig = json.load(f)
             self.token = userConfig['account']['token']
             self.url = userConfig['account']['url']
+            if userConfig['account']['type'] == 'ac':
+                self.cacheFunction = self._syncProjectsAC
+            else:
+                self.cacheFunction = self._syncProjectsFB
 
     def run(self):
-        self.conn = sqlite3.connect(self.dataPath,
-            detect_types=sqlite3.PARSE_DECLTYPES)
-        self.conn.row_factory = sqlite3.Row
         try:
             self._initDB()
             return {
@@ -50,7 +53,7 @@ class PyTimer:
                 'show': self._showTimer,
                 'edit': self._editTimer,
                 'assign': self._assignGroup,
-                'projects': self._listProjects
+                'find': self._findProject
             }.get(self.op, self._noOp)()
         finally:
             self.conn.close()
@@ -62,7 +65,7 @@ class PyTimer:
             # If row is not None and groupId is still -1, we need to create this group
             if (self.group and groupId == -1):
                 self.conn.execute('''INSERT INTO groups(name)
-                        VALUES (?)''', self.group)
+                        VALUES (?)''', [ self.group ])
                 groupId = self._findGroupId(self.group)
 
             self.conn.execute('''INSERT INTO timers(name, note, start, group_id)
@@ -76,13 +79,19 @@ class PyTimer:
                 (self._roundTime(datetime.utcnow()), self.name))
 
     def _showTimer(self):
-        query = '''SELECT id, name, note, start, end FROM timers'''
+        query = 'SELECT id, name, note, start, end FROM timers'
+        where = ''
         params = list()
-        if (self.name == 'all'):
-            query += ' WHERE end IS NULL'
-        else:
-            query += ' WHERE name LIKE ?'
+        if not self.showAll:
+            where += 'end IS NULL'
+
+        if (self.name != '*'):
+            if where: where += ' AND '
+            where += 'name LIKE ?'
             params.append(self.name)
+
+        if (where):
+            query = query + ' WHERE ' + where
 
         for row in self.conn.execute(query, params):
             utc = row['start'].replace(tzinfo=tz.UTC)
@@ -105,16 +114,38 @@ class PyTimer:
                 ticket_id = ? WHERE name LIKE ?''',
                 (self.project, self.ticket, self.name))
 
-    def _listProjects(self):
-        if self.name == 'all':
-            req = ACRequest('projects', api_key=self.token, ac_url=self.url)
-            for url in req.execute():
-                print(type(url), '=>', url)
+    def _findProject(self):
+        self._syncProjects()
+        if (self.type == 'projects'):
+            # Do stuff for projects here
+            pass
+        elif (self.type == 'tickets'):
+            # Do stuff for tickets here
+            pass
+        else:
+            # Do stuff for searching both here
+            pass
 
     def _noOp(self):
         print('There is no defined operation for the command %s.' % self.op)
 
     def _initDB(self):
+        needsSchemaUpgrade = not path.exists(self.dataPath)
+
+        self.conn = sqlite3.connect(self.dataPath,
+            detect_types=sqlite3.PARSE_DECLTYPES)
+        self.conn.row_factory = sqlite3.Row
+
+        if not needsSchemaUpgrade: 
+            curs = self.conn.execute('''SELECT max(sync_date) as "[sync_date timestamp]" FROM (
+                SELECT sync_date FROM tickets t 
+                UNION ALL
+                SELECT sync_date FROM projects p
+            ) ''')
+            row = curs.fetchone();
+            self.lastSynced = row[0]
+            return
+
         with self.conn:
             self.conn.execute('''
             CREATE TABLE IF NOT EXISTS timers
@@ -126,17 +157,59 @@ class PyTimer:
                 (id integer PRIMARY KEY AUTOINCREMENT, name text NOT NULL,
                  project_id integer, ticket_id integer)
             ''')
+            self.conn.execute('''
+            CREATE TABLE IF NOT EXISTS projects
+                (id integer PRIMARY KEY, name text NOT NULL, 
+                sync_date timestamp DEFAULT CURRENT_TIMESTAMP)
+            ''')
+            self.conn.execute('''
+            CREATE TABLE IF NOT EXISTS tickets
+                (id integer PRIMARY KEY, project_id integer, name text NOT NULL, 
+                sync_date timestamp DEFAULT CURRENT_TIMESTAMP)
+            ''')
+
+    def _syncProjects(self):
+        if (self.url == None or self.token == None or self.cacheFunction == None):
+            raise RuntimeError('Either url, token, or accountType is NULL, check config file')
+
+        # The cache is valid, return
+        if (not self.lastSynced or datetime.utcnow() - self.lastSynced < timedelta(hours=CACHE_LIFETIME)):
+            self.cacheFunction()
+
+        self.lastSync = datetime.utcnow()
+    
+    def _syncProjectsAC(self):
+        with self.conn:
+            req = ACRequest('projects', ac_url=self.url, api_key=self.token)
+            for project in req.execute():
+                print(project)
+                self.conn.execute('''
+                    INSERT OR REPLACE INTO projects(id, name) VALUES (?, ?)
+                ''', (project['id'], project['name']))
+
+                req = ACRequest('projects', item_id=project['id'], subcommand='tickets', 
+                    ac_url=self.url, api_key=self.token)
+
+                for ticket in req.execute():
+                    print(ticket)
+                    self.conn.execute('''
+                        INSERT OR REPLACE INTO tickets(id, project_id, name) VALUES (?, ?, ?)
+                    ''', (ticket['ticket_id'], project['id'], ticket['name']))
+
+
+    def _syncProjectsFB(self):
+        pass
 
     def _findGroupId(self, group):
         groupId = -1
         curs = self.conn.execute('''SELECT id FROM groups
-                WHERE name LIKE ?''', group)
+                WHERE name LIKE ?''', [ group ])
         row = curs.fetchone()
         if (row):
             groupId = row[0]
         return groupId
 
-    def _roundTime(dt=None, roundTo=60):
+    def _roundTime(self, dt=None, roundTo=60):
         if dt == None:
             dt = datetime.utcnow()
 
@@ -164,8 +237,9 @@ def main():
     parser_end.add_argument('name', help='Name of the timer to be stopped')
 
     parser_show = subparsers.add_parser('show', help='List all running timers')
-    parser_show.add_argument('name', nargs='?', default='all',
-        help='Specify a timer to show details about')
+    parser_show.add_argument('name', nargs='?', default='*', help='Specify a timer to show details about')
+    parser_show.add_argument('-a', '--all', dest='showAll', action='store_true', default=False, 
+        help='Show all timers.  The default behaviour is to show only running timers')
 
     parser_edit = subparsers.add_parser('edit', help='Edit a stopped timer')
     parser_edit.add_argument('name', help='Specify a timer to show details about')
@@ -179,10 +253,11 @@ def main():
     parser_assign.add_argument('project', help='Project id to assign group to')
     parser_assign.add_argument('ticket', help='Ticket id to assign group to')
 
-    parser_projects = subparsers.add_parser('projects',
-        help='Show details about projects')
-    parser_projects.add_argument('name', nargs='?', default='all',
-        help='Specify a timer to show details about')
+    parser_projects = subparsers.add_parser('find', help='Show details about projects')
+    parser_projects.add_argument('name', help='Name to search for')
+    parser_projects.add_argument('-t', '--type', default='*', choices=['*', 'ticket', 'project'],
+        help='If we should look for projects, tickets or both (defaults to both)')
+    
 
     config = parser.parse_args()
 
@@ -191,7 +266,7 @@ def main():
         parser.print_help()
         return -1
 
-    PyTimer(config).run()
+    QTimer(config).run()
 
     return 0
 
