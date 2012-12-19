f7e065a874cc8a2bd3ed4ec51b2f8f3a7e7262de
Jon Monroe <berkona@gmail.com>
2012-12-18 22:42:37 -0500

    Added timezone handling, unique ID for timers, editing for timers

--- qtimer.py
+++ qtimer.py
@@ -1,17 +1,20 @@
 #! /usr/bin/env python
 
-import freshbooks
+# import freshbooks
 import sqlite3
 import argparse
-import json
+import tz
 
-from activecollab.library import ACRequest
-from datetime import datetime
+# import json
 
+# from activecollab.library import ACRequest
+from datetime import datetime, timedelta
 from os import path
 
-CONFIG_NAME = 'config.json'
-DATA_NAME = 'timers.db'
+DB_VERSION = 2
+CONFIG_NAME = 'qtimer.json'
+DATA_NAME = 'timers v%d.db' % DB_VERSION
+
 
 class PyTimer:
     def __init__(self, config):
@@ -22,30 +25,32 @@ class PyTimer:
         self.dataPath = path.join(scriptRoot, DATA_NAME)
 
     def run(self):
-        self.conn = sqlite3.connect(self.dataPath)
+        self.conn = sqlite3.connect(self.dataPath,
+            detect_types=sqlite3.PARSE_DECLTYPES)
         self.conn.row_factory = sqlite3.Row
         try:
             self._initDB()
-            retVal = {
+            return {
                 'start': self._startTimer,
                 'end': self._endTimer,
-                'status': self._getTimerStatus,
+                'show': self._showTimer,
+                'edit': self._editTimer
             }.get(self.op, self._noOp)()
         finally:
             self.conn.close()
 
     def _startTimer(self):
         with self.conn:
-            self.conn.execute('INSERT INTO timers(name, note, start) VALUES (?, ?, ?) ', 
-                (self.name, self.note, datetime.utcnow()))
+            self.conn.execute('INSERT INTO timers(name, note, start) VALUES (?, ?, ?) ',
+                (self.name, self.note, self._roundTime(datetime.utcnow())))
 
     def _endTimer(self):
         with self.conn:
             self.conn.execute('UPDATE timers SET end = ? WHERE name LIKE ? AND end IS NULL',
-                (datetime.utcnow(), self.name))
+                (self._roundTime(datetime.utcnow()), self.name))
 
-    def _getTimerStatus(self):
-        query = 'SELECT name, note, start, end FROM timers'
+    def _showTimer(self):
+        query = '''SELECT id, name, note, start, end FROM timers'''
         params = list()
         if (self.name == 'all'):
             query += ' WHERE end IS NULL'
@@ -53,21 +58,42 @@ class PyTimer:
             query += ' WHERE name LIKE ?'
             params.append(self.name)
 
-        i = 0
         for row in self.conn.execute(query, params):
-            timer = []
-            for val in row:
-                timer.append(str(val))
-            print(i, ': ', ', '.join(timer))
-            i += 1
+            formattedStart = row['start'].replace(tzinfo=tz.UTC).astimezone(tz.Local).strftime('%x %H:%M')
+            end = row['end'] if row['end'] else datetime.utcnow()
+            duration = self._roundTime(end - row['start'])
+            print('%s(%d): %s %s %s'
+                % (row['name'], row['id'], formattedStart, duration,
+                    row['note'] if row['note'] else ''))
+
+    def _editTimer(self):
+        with self.conn:
+            self.conn.execute('UPDATE timers SET note = ?, start = ?, end = ? WHERE name LIKE ?',
+                (self.note, self.start, self.end, self.name))
 
     def _noOp(self):
         print('There is no defined operation for the command %s.' % self.op)
 
     def _initDB(self):
         with self.conn:
-            self.conn.execute('''CREATE TABLE IF NOT EXISTS timers 
-                (name text PRIMARY KEY NOT NULL, note text, start date, end date) ''')
+            self.conn.execute(
+            '''CREATE TABLE IF NOT EXISTS timers
+                (id integer PRIMARY KEY AUTOINCREMENT, name text NOT NULL,
+                    note text, start timestamp, end timestamp) ''')
+
+    def _roundTime(dt=None, roundTo=60):
+        if dt == None:
+            dt = datetime.utcnow()
+
+        seconds = (dt - dt.min).seconds
+        # // is a floor division not a comment on the following line
+        rounding = (seconds + roundTo / 2) // roundTo * roundTo
+        ms = dt.microseconds if hasattr(dt, 'microseconds') else 0
+        return dt + timedelta(0, rounding - seconds, -ms)
+
+
+def parseTime(dateStr):
+    return datetime.strptime(dateStr, '%Y-%m-%d %H:%M')
 
 
 def main():
@@ -80,15 +106,21 @@ def main():
 
     parser_end = subparsers.add_parser('end', help='End a currently running timer')
     parser_end.add_argument('name', help='Name of the timer to be stopped')
-    
-    parser_status = subparsers.add_parser('status', help='List all running timers')
-    parser_status.add_argument('name', nargs='?', default='all', 
+
+    parser_show = subparsers.add_parser('show', help='List all running timers')
+    parser_show.add_argument('name', nargs='?', default='all',
         help='Specify a timer to show details about')
+
+    parser_edit = subparsers.add_parser('edit', help='Edit a stopped timer')
+    parser_edit.add_argument('name', help='Specify a timer to show details about')
+    parser_edit.add_argument('-n', '--note', help='Note to set for this timer')
+    parser_edit.add_argument('-s', '--start', type=parseTime, help='Start date to set for this timer')
+    parser_edit.add_argument('-e', '--end', type=parseTime, help='Duration to set for this timer')
 
     config = parser.parse_args()
 
     # Check that we have a task to do
-    if not hasattr(config, 'op'):
+    if not config.op:
         parser.print_help()
         return -1
 
