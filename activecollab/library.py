34030e9d21daa2cf57ec43556142c8a3fdf7a1ae
Jon Monroe <berkona@gmail.com>
2012-12-19 08:41:38 -0500

    Added more useful output to ACRequest

--- activecollab/library.py
+++ activecollab/library.py
@@ -1,9 +1,12 @@
-import urllib
 import re
+
+from urllib.parse import urlencode
+from urllib.request import urlopen
 from xml.dom.minidom import parse
 from activecollab.settings import API_KEY, AC_URL
 from activecollab.constants import *
 from activecollab.exceptions import ACCommandException
+
 
 class ACRequest(object):
     """ Makes a request to your Active Collab site and executes
@@ -14,8 +17,8 @@ class ACRequest(object):
         if (command not in AC_COMMANDS):
             raise ACCommandException('Not a valid command')
         if subcommand and (subcommand not in AC_SUBCOMMAND):
-            raise ACCommandException('Not a valid subcommand %s'% str(AC_SUBCOMMAND))
-        if (subcommand and not item_id): 
+            raise ACCommandException('Not a valid subcommand %s' % str(AC_SUBCOMMAND))
+        if (subcommand and not item_id):
             raise ACCommandException('Subcommands require a top level id')
 
         self.command = command
@@ -24,8 +27,8 @@ class ACRequest(object):
         self.subcommand = subcommand
         self.api_key = kwargs.get('api_key', API_KEY)
         self.ac_url = kwargs.get('ac_url', AC_URL)
-        self.params = urllib.urlencode(kwargs.get('params', dict()))
-        
+        self.params = urlencode(kwargs.get('params', dict()))
+
         # quick n easy tag clean - nuffin' fancy
         self.striptags = re.compile(r'<.*?>')
 
@@ -34,12 +37,12 @@ class ACRequest(object):
             try:
                 self.valid_fields += AC_SUB_FIELDS[self.subcommand]
             except KeyError:
-                pass # no need to fail here if not defined
+                pass  # no need to fail here if not defined
 
     @property
     def base_url(self):
         """ Build our base API request URL"""
-        return 'http://%s/api.php?token=%s&path_info=' % \
+        return 'https://%s/api.php?token=%s&path_info=' % \
             (self.ac_url, self.api_key)
 
     @property
@@ -66,7 +69,7 @@ class ACRequest(object):
     def execute(self):
         """ Make a request for the XML and parse the response """
         try:
-            raw_xml = urllib.urlopen(self.command_url)
+            raw_xml = urlopen(self.command_url)
         except:
             raise ACCommandException('Could not execute command')
 
@@ -79,22 +82,21 @@ class ACRequest(object):
 
         output = list()
         for item in items:
-            item_str = ''
+            item_dict = {}
             for node in item.childNodes:
-
-                if self.sub_id and self.subcommand == 'tickets': 
+                if self.sub_id and self.subcommand == 'tickets':
                     # If we have a ticket id and we're actually looking at a ticket
                     # then show some more useful info instead of a list
                     if node.localName == 'body':
-                        item_str += self.striptags.sub('', node.childNodes[0].nodeValue)
+                        item_dict[node.localName] = self.striptags.sub('',
+                            node.childNodes[0].nodeValue)
                         break
 
                 else:
                     # 'standard' output of the fields so for the current block
                     if node.localName in self.valid_fields:
-                        item_str += node.childNodes[0].nodeValue + AC_FIELD_SEP
+                        item_dict[node.localName] = node.childNodes[0].nodeValue
 
-            output.append(item_str)
+            output.append(item_dict)
 
-        return [o.rstrip(AC_FIELD_SEP) for o in output]
-
+        return output
