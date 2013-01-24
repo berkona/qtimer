9540e4c25d6b0fe956c976aa18caed3d73e3f6ee
Berkona <berkona@gmail.com>
2013-01-24 11:28:37 -0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
 	scripts=['bin/qtimer', 'bin/qtimer-gui'],
 	license='LICENSE.txt',
 	description='A small timer program that integrates with various project management solutions',
-	long_description=open('README.txt').read(),
+	long_description=open('README.md').read(),
 	install_requires=[
 		'sqlalchemy >= 0.7.9',
 		'alembic >= 0.4.1',
