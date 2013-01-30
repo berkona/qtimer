46e4eb4ee1f5d493195638c2950c29f1a19b62e5
Berkona <berkona@gmail.com>
2013-01-30 18:56:47 -0500

    Reordered keys in ini, added better translateDate function for TimerFilterDelegate

--- qtimerui/model.py
+++ qtimerui/model.py
@@ -11,6 +11,69 @@ from qtimer.util import *
 LOGGER = logging.getLogger(__name__)
 
 
+# duck-typing to avoid unintended inheritance
+class TimerIndexProxy(object):
+
+	def __init__(self, realObject):
+		self.wrapped = realObject
+
+		# We have to keep this in state memory b/c we need a reference to ourselves
+		self.translationFuncs = [
+			lambda t: t.status.title(),
+			lambda t: t.name,
+			lambda t: format_time(t.start),
+			lambda t: str(self.backend.roundTime(t.duration)),
+			lambda t: "Posted" if t.posted else "Not posted",
+		]
+
+	def __eq__(self, other):
+		return self.wrapped.__eq__(other)
+
+	def __lt__(self, other):
+		return self.wrapped.__lt__(other)
+
+	def __ne__(self, other):
+		return self.wrapped.__ne__(other)
+
+	def child(self, row, column):
+		return self.wrapped.child(row, column)
+
+	def column(self):
+		return self.wrapped.column()
+
+	def data(self, role=Qt.DisplayRole):
+		data = self.wrapped.data(role)
+		if not (role == Qt.DisplayRole):
+			return data
+
+		LOGGER.info('timer: %r', data)
+		return self.translationFuncs[self.column()](data)
+
+	def flags(self):
+		return self.wrapped.flags()
+
+	def internalId(self):
+		return self.wrapped.internalId()
+
+	def internalPointer(self):
+		return self.wrapped.internalPointer()
+
+	def isValid(self):
+		return self.wrapped.isValid()
+
+	def model(self):
+		return self.wrapped.model()
+
+	def parent(self):
+		return self.wrapped.parent()
+
+	def row(self):
+		return self.wrapped.row()
+
+	def sibling(self, row, column):
+		return self.wrapped.sibling(row, column)
+
+
 class FilterDelegate(QObject):
 
 	filterChanged = Signal()
@@ -27,12 +90,26 @@ class FilterDelegate(QObject):
 	def isActive(self, item):
 		return False
 
-	def translateData(self, data, column):
-		# No op
-		return data
+
+class TimerDelegate(QStyledItemDelegate):
+
+	def __init__(self, backend, parent=None):
+		super(TimerDelegate, self).__init__(parent)
+
+	def paint(self, painter, option, index):
+		# Translate all calls through a proxy
+		super(QAbstractItemDelegate, self).paint(painter, option, TimerIndexProxy(index))
 
 
 class TimerFilterDelegate(FilterDelegate):
+
+	textHeader = [
+		'Status',
+		'Name',
+		'Start Time',
+		'Duration',
+		'Posted'
+	]
 
 	def __init__(self, backend):
 		super(TimerFilterDelegate, self).__init__()
@@ -74,24 +151,6 @@ class TimerFilterDelegate(FilterDelegate):
 			query = query.join(Ticket).filter(Ticket.project_id == self.projectId)
 
 		return query
-
-	def translateData(self, data, column):
-		translationFuncs = [
-			lambda t: t.status.title(),
-			lambda t: t.name,
-			lambda t: format_time(t.start),
-			lambda t: str(self.backend.roundTime(t.duration)),
-			lambda t: str(t.posted),
-		]
-		return translationFuncs[column](data)
-
-	textHeader = [
-		'Status',
-		'Name',
-		'Start Time',
-		'Duration',
-		'Posted'
-	]
 
 	def headerData(self, section, orientation, role=Qt.DisplayRole):
 		if role == Qt.TextAlignmentRole:
@@ -161,7 +220,7 @@ class ORMListModelAdapter(QAbstractTableModel):
 		return self._cache
 
 	def flags(self, index):
-		return Qt.ItemIsSelectable | Qt.ItemIsEnabled
+		return Qt.ItemIsSelectable | Qt.ItemIsEnabled | Qt.ItemIsEditable
 
 	def headerData(self, section, orientation, role=Qt.DisplayRole):
 		return self.delegate.headerData(section, orientation, role)
@@ -178,7 +237,13 @@ class ORMListModelAdapter(QAbstractTableModel):
 		elif role == Qt.TextAlignmentRole:
 			return int(Qt.AlignLeft | Qt.AlignVCenter)
 
-		return self.delegate.translateData(self.getItem(index), index.column())
+		return self.getItem(index)
+
+	def setData(self, index, value, role=Qt.EditRole):
+		if not (role == Qt.EditRole):
+			return False
+
+		return True
 
 	def rowCount(self, parent=QModelIndex()):
 		LOGGER.debug('rowCount', self.cache.size)
