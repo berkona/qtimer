92e7054ca8d466014d6c90f8f483cd5f704faa4a
Berkona <berkona@gmail.com>
2013-02-02 16:15:45 -0500

    Added better filter in end timer

--- qtimer/commands/end_timer.py
+++ qtimer/commands/end_timer.py
@@ -21,12 +21,9 @@ class EndTimer(Command):
 				Session.end: core.roundTime(datetime.utcnow())
 			}
 
-			remove_tuples = lambda row: row[0]
-			ids = map(remove_tuples, session.query(Timer.id).filter(
-				Timer.name.like('%' + args['name'] + '%')).all())
-
-			session.query(Session).filter(Session.timer_id.in_(ids))\
-				.filter(Session.end == None).update(values, 'fetch')
+			session.query(Session).join(Timer).filter()\
+			.filter(Timer.name.like('%' + args['name'] + '%'))\
+			.filter(Session.end == None).update(values, 'fetch')
 
 		args = program.parseArgs([ 'find', 'timers', '-n', args['name'] ])
 		return program.executeCommand(args)
