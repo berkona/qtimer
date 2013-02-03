4df74e4802ab7271baf50ed15641a7d2597fb068
Berkona <berkona@gmail.com>
2013-02-02 20:05:17 -0500

    Added post-install hook for setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,32 @@
 from distutils.core import setup
+from distutils.command.install import install
 from distutils.dir_util import copy_tree
 
 from os import makedirs, path
 
 from qtimer.env import DATA_DIR, SCRIPT_ROOT
 
+
+class CopySchemaPostInstall(install):
+	def run(self):
+		install.run(self)
+
+		print("Copying schema information to user data directory")
+		schemaDest = path.join(DATA_DIR, 'schema')
+		if not path.exists(schemaDest):
+			makedirs(schemaDest)
+		copy_tree(path.join(SCRIPT_ROOT, 'schema'), schemaDest)
+
+
 setup(
 	name='qTimer',
 	version='0.1.0',
 	author='Jon "Berkona" Monroe',
 	author_email='solipsis.dev@gmail.com',
+	url='https://github.com/berkona/qtimer',
 	packages=[
-		'qtimer', 'qtimer.lib', 'qtimer.commands',
-		'qtimer.plugins', 'qtimer.schema',
-		'qtimer.schema.versions',
+		'qtimer', 'qtimer.lib',
+		'qtimer.commands', 'qtimer.plugins',
 	],
 	scripts=['bin/qtimer', ],
 	license='LICENSE.txt',
@@ -24,13 +37,5 @@ setup(
 		'alembic >= 0.4.1',
 		'appdirs >= 1.2.0'
 	],
+	cmdclass=dict(install=CopySchemaPostInstall),
 )
-
-print("Copying schema information to user data directory")
-
-schemaDest = path.join(DATA_DIR, 'schema')
-
-if not path.exists(schemaDest):
-	makedirs(schemaDest)
-
-copy_tree(path.join(SCRIPT_ROOT, 'schema'), schemaDest)
